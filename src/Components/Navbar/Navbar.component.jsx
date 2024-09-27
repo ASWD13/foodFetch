b3690794ff417a3de6870372d113c72f770fedc0
Rohan Karankot <rohankarankot@gmail.com>
2024-09-27 20:46:48 +0530

    created login and register component --exploiter

--- src/Components/Navbar/Navbar.component.jsx
+++ src/Components/Navbar/Navbar.component.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import LoginComponent from "../Login/Login.component";
 import ModalComponent from "../Modal/Modal.component";
 
 import { Button } from "../ui/button"
@@ -10,10 +9,15 @@ import {
   DialogHeader,
   DialogTrigger,
 } from "../ui/dialog"
+import { BiMenu } from "react-icons/bi";
+import { CgClose } from "react-icons/cg";
+import RegisterComponent from "../Auth/register.component";
+import { LoginComponent } from "../Auth/login.component";
 
 function NavbarComponent() {
   const [showLogin, setShowLogin] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [authScreen, setAuthScreen] = useState("login");
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -36,30 +40,52 @@ function NavbarComponent() {
             />
           </div>
           <div className=" w-1/3 flex justify-center align-middle ">
-
-
             <a className="p-2 hover:text-white text-white">First Link</a>
             <a className="p-2 hover:text-white text-white">First Link</a>
             <a className="p-2 hover:text-white text-white">First Link</a>
           </div>
           <div className=" w-1/6 flex justify-center align-middle ">
-
             <DialogTrigger>
               <Button variant="link" className="text-white" onClick={toggleMenu}>login</Button>
-
             </DialogTrigger>
           </div>
+        </div>
 
+        <div className="bg-slate-900 md:hidden">
+          <div className="flex justify-center p-4">
+            {!menuOpen ? <BiMenu className="text-white absolute top-4 right-4" size="30" onClick={toggleMenu} /> :
+              <CgClose className="text-white absolute top-4 right-4" size="30" onClick={toggleMenu} />}
+            <span className="ml-3 text-xl xl:block text-white">
+              {"</>"}Food_Fetch
+            </span>
+          </div>
+          {menuOpen &&
+            <div className="  flex flex-col justify-center align-middle ">
+              <a className="p-2 hover:text-white text-white">First Link</a>
+              <a className="p-2 hover:text-white text-white">First Link</a>
+              <a className="p-2 hover:text-white text-white">First Link</a>
+
+              <div className=" w-1/6 flex justify-center align-middle ">
+                <DialogTrigger>
+                  <Button variant="link" className="text-white" onClick={toggleMenu}>login</Button>
+                </DialogTrigger>
+              </div>
+            </div>}
         </div>
 
 
 
-
         <DialogContent className="h-full" >
-          <DialogHeader>
-            <h2 className="text-gray-900 text-3xl font-medium title-font mb-5 text-center">Register</h2>
-          </DialogHeader>
-          <LoginComponent />
+          <h2 className="text-gray-900 text-3xl font-medium title-font mb-5 text-center">
+            {authScreen == "login" ? "Welcome Back Foodiee" : "Lets Register"}
+          </h2>
+          {authScreen == "login" ? <LoginComponent /> : <RegisterComponent />}
+          <p className="text-xs text-gray-500 ">
+
+            {authScreen == "login" ? <span onClick={() => setAuthScreen("register")}>click here to register</span> : <span onClick={() => setAuthScreen("login")}>back to login</span>}
+
+          </p>
+
         </DialogContent>
 
 
