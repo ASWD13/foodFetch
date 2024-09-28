9f9b54e5286ec28e9832f56f36fda49a5a9cd12b
Rohan Karankot <rohankarankot@gmail.com>
2024-09-28 13:05:44 +0530

    remaned to js

--- src/Components/Trending/hotel.card.component.jsx
+++ src/Components/Trending/hotel.card.component.jsx
@@ -6,7 +6,7 @@ const HotelCardComponent = ({ hotelData }) => {
     const { Name, Rating, Description, Address, Image: data } = hotelData
     console.log('data: ', data?.data?.[0]?.attributes?.url);
     return (
-        <div className="border border-primary w-[250px] p-2">
+        <div className="border border-primary w-[250px] p-2 " data-aos="flip-up">
             <img src={data?.data?.[0]?.attributes?.url || "https://media.istockphoto.com/id/104731717/photo/luxury-resort.jpg?s=612x612&w=0&k=20&c=cODMSPbYyrn1FHake1xYz9M8r15iOfGz9Aosy9Db7mI="} alt="" className="w-[250px] h-[200px] object-cover" />
             <p>{Name}</p>
             <p>{Description}</p>
---
+++ src/utils/apiConfig.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL; //process.env.VITE_BASE_URL
+
+export const appAxios = axios.create({
+  baseURL: BASE_URL,
+});
