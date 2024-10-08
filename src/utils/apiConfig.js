34055cea1a693beb348d39c708011cad7ed02a4b
Rohan Karankot <rohankarankot@gmail.com>
2024-10-08 21:51:56 +0530

    used prod base url

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const appAxios = axios.create({
-  baseURL: "http://localhost:1337/api", // BASE_URL,
+  baseURL: BASE_URL,
 });
 
 const excludeAuthRoutes = ["/auth/local", "/auth/local/register"];
